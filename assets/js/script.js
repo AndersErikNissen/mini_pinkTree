a732d5f93eb2500652faca54ad678db0c03e1774
ucn-aen <73883535+ucn-aen@users.noreply.github.com>
2021-08-01 03:02:59 +0200

    update
    Trainride - 31 july
    
    Working with diffrent ways to animate sparks.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,23 +2,30 @@
 //UCN MMDA0920
 
 
-console.log("               ____________                _____________________________    ________            _______")
-console.log("              /            \\              |                             |  |         \\         |       |")
-console.log("             /              \\             |                             |  |          \\        |       |");
-console.log("            /                \\            |         ____________________|  |           \\       |       |")
-console.log("           /        /\\        \\           |        |                       |            \\      |       |")
-console.log("          /        /  \\        \\          |        |                       |             \\     |       |")
-console.log("         /        /    \\        \\         |        |____________________   |              \\    |       |")
-console.log("        /        /      \\        \\        |                             |  |               \\   |       |")
-console.log("       /        /________\\        \\       |                             |  |       |\\       \\  |       |")
-console.log("      /                            \\      |         ____________________|  |       | \\       \\ |       |")
-console.log("     /        _____________         \\     |        |                       |       |  \\       \\|       |")
-console.log("    /        /             \\         \\    |        |                       |       |   \\               |")
-console.log("   /        /               \\         \\   |        |____________________   |       |    \\              |")
-console.log("  /        /                 \\         \\  |                             |  |       |     \\             |")
-console.log(" /        /                   \\         \\ |                             |  |       |      \\            |")
-console.log("/________/                     \\_________\\|_____________________________|  |_______|       \\___________|")
 
+// window.addEventListener("load", () => {
+//     const pink = document.querySelector("#pinkTree").contentDocument;
+    
+//     function move1 () {
+//         const
+//         move = pink.querySelector("#move"), 
+//         dot1 = pink.querySelector("#grp1");
+//         let x, y;
+        
+//         // move.style.position = "relative";
+//         // move.style.height = "100%";
+//         // move.style.width = "100%";
+//         // dot1.style.position = "absolute";
 
+//         for(let i = 0; i < 1; i+= 0.1) {
+//             x = i * 10;
+//             y = (i * i) * 10;
+            
+//             dot1.style.transform = "transform(" + x + "%" + y + "%" + ")";
+//             console.log(x, y)
+//             console.log( dot1.style.transform = "translate(" + x + "%" + "," + y + "%" + ")")
+//         }
+//     }
+//     move1();
 
- 
+// })
