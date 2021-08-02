c043275474a1fd85f1290ce3fa75a97eaf24659d
ucn-aen <73883535+ucn-aen@users.noreply.github.com>
2021-08-02 03:54:23 +0200

    start with Main Content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,26 @@
 //JS by Anders Erik Nissen
 //UCN MMDA0920
 
+// Up Btn
+const 
+upBtn = document.querySelector("#arrowUp"),
+content = document.querySelector("#mainContent");
 
+upBtn.addEventListener("click", () => {
+    let
+    one = content.classList.contains("inchght"),
+    two = content.classList.contains("dehght");
 
-// window.addEventListener("load", () => {
-//     const pink = document.querySelector("#pinkTree").contentDocument;
-    
-//     function move1 () {
-//         const
-//         move = pink.querySelector("#move"), 
-//         dot1 = pink.querySelector("#grp1");
-//         let x, y;
-        
-//         // move.style.position = "relative";
-//         // move.style.height = "100%";
-//         // move.style.width = "100%";
-//         // dot1.style.position = "absolute";
-
-//         for(let i = 0; i < 1; i+= 0.1) {
-//             x = i * 10;
-//             y = (i * i) * 10;
-            
-//             dot1.style.transform = "transform(" + x + "%" + y + "%" + ")";
-//             console.log(x, y)
-//             console.log( dot1.style.transform = "translate(" + x + "%" + "," + y + "%" + ")")
-//         }
-//     }
-//     move1();
-
-// })
+    switch(true) {
+        case one:
+        content.classList.remove("inchght");
+        content.classList.add("dehght");
+        break;
+        case two:
+        content.classList.remove("dehght");
+        content.classList.add("inchght");
+        break;
+        default:
+        content.classList.add("inchght");
+    }
+})
