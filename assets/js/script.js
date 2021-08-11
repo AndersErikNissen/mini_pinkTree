22c2d5a331953fa0e9cb1cb8752dd8cea8beee5d
Anders <1086088@ucn.dk>
2021-08-11 16:49:33 +0200

    Colorpicker = Done + Start on MainMain
    - Might need another color scheme?

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,7 +50,10 @@ introBtn = document.querySelector("#intro_btn"),
 step1 = document.querySelector("#step1"),
 step1btn = document.querySelector("#step1_nextBtn"),
 
-step2 = document.querySelector("#step2");
+step2 = document.querySelector("#step2"),
+step2btn = document.querySelector("#step2_nextBtn"),
+
+mainMain = document.querySelector("#mainMain");
 
 let
 //Intro > Step1
@@ -59,7 +62,10 @@ intDisplay = "--intro-display",
 stepOneDisplay = "--step-one-display",
 //Step1 > Step2
 stepOne = "#step1",
-stepTwoDisplay = "--step-two-display";
+stepTwoDisplay = "--step-two-display",
+//Step2 > MainMain
+stepTwo = "#step2",
+mainMainDisplay = "--mainMain-display";
 
 
 function nextStep (step, var1, var2, sec_step) {
@@ -86,21 +92,71 @@ introBtn.addEventListener("click", () => {
     nextStep(intro, intDisplay, stepOneDisplay, step1);
 })
 step1btn.addEventListener("click", ()=> {
-    let data = document.querySelector("#step1").getAttribute("data-status");
+    nextStep(stepOne, stepOneDisplay, stepTwoDisplay, step2);
 
-    if (data === "0") {
-        alert("Please Select!")
+    let 
+    data = document.querySelector("#step1").getAttribute("data-status"),
+    dataNR = parseInt(data);
+
+    // Changes the corners on elements depending on the attribute.
+    if (dataNR === 1) {
+        root.style.setProperty("--corners", "var(--theme-edge)")
     } else {
-        nextStep(stepOne, stepOneDisplay, stepTwoDisplay, step2);
+        root.style.setProperty("--corners", "0px")
     }
+});
+step2btn.addEventListener("click", () => {
+    nextStep(stepTwo, stepTwoDisplay, mainMainDisplay, mainMain);
+
+    // If statements for Both Themes, to set the variables for the mainMain content.
+    let
+    step_1 = document.querySelector("#step1").getAttribute("data-status"),
+    step_1NR = parseInt(step_1),
+    
+    step_2 = document.querySelector("#step2").getAttribute("data-status"),
+    step_2NR = parseInt(step_2);
+    
+    //Looks at Step 1, if === 1 have round edges, else sharp corners.
+    if (step_1NR === 1) {
+        root.style.setProperty("--mainMain-edge", "var(--theme-edge)")
+    } else {
+        root.style.setProperty("--mainMain-edge", "0")
+    }
+
+    //Changes the Pickers Colors if Pink(2) is selected otherwise nothing needs to change.
+    if (step_2NR === 2) {
+        root.style.setProperty("--picker-color-one", "rgb(255, 236, 249)");
+        root.style.setProperty("--picker-color-two", "linear-gradient(346deg, rgba(42,224,45,1) 0%, rgba(48,190,244,1) 100%)");
+        root.style.setProperty("--picker-color-bg", "#ce1e58");
+    } 
+    root.style.setProperty("--mainMain-color-one", "var(--picker-color-one)");
+    root.style.setProperty("--mainMain-color-two", "var(--picker-color-two)");
+    root.style.setProperty("--mainMain-color-bg", "var(--picker-color-bg)");
+
+    
+    document.querySelector("#mainMain_welcome").classList.add("welcome");
+
+    
+    setTimeout(()=> {document.querySelector("#topX").style.display = "none";}, 900)
+    setTimeout(()=> {
+        document.querySelector("#welcomeDiv").style.display = "none";
+    }, 6000)
+    setTimeout(()=> {
+        document.querySelector("#mainMain").style.justifyContent = "start";
+        document.querySelector("#mainMain_content").classList.add("welcome_mainMain");
+    }, 6200)
+
 })
 
 
 
-// Select Theme
+
+// Select Theme / Color Picker
 const
 soft = document.querySelector("#theme1"),
-edge = document.querySelector("#theme2");
+edge = document.querySelector("#theme2"),
+blue = document.querySelector("#dark_theme"),
+pink = document.querySelector("#light_theme");
 
 soft.addEventListener("click", () => {
     edge.classList.remove("actTheme");
@@ -120,6 +176,35 @@ edge.addEventListener("click", () => {
     step1.setAttribute("data-status", 2);
     step1btn.classList.add("showNextBtn");
 })
+
+blue.addEventListener("click", () => {
+    let 
+    h3 = document.querySelector("#dark_theme h3"),
+    h3other = document.querySelector("#light_theme h3");
+
+    step2.setAttribute("data-status", 1);
+    pink.classList.remove("actTheme");
+    blue.classList.add("actTheme");
+    h3.style.color = "black";
+    h3other.style.color = "white";
+    step2btn.classList.add("showNextBtn");
+})
+pink.addEventListener("click", () => {
+    let 
+    h3 = document.querySelector("#light_theme h3"),
+    h3other = document.querySelector("#dark_theme h3");
+
+    step2.setAttribute("data-status", 2);
+    pink.classList.add("actTheme");
+    blue.classList.remove("actTheme");
+    h3.style.color = "black";
+    h3other.style.color = "white";
+    step2btn.classList.add("showNextBtn");
+})
+
+
+
+
 
 // Close Container
 const
